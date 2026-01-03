d8546f534274dd1179bc8e2a05679cf8d33b92a3
Ezeki Zibzibadze <ez@jok.io>
2026-01-03 22:14:23 +0400

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,58 @@
-import type { Router } from "express";
-import type { InferRouteConfig, RouterConfig } from "./types";
+import type { Router } from "express"
+import type { InferRouteConfig, RouterConfig } from "./types"
 
 export type RouterHandlerConfig<T extends RouterConfig> = {
   GET: {
     [K in keyof T["GET"]]: (
       data: Omit<Omit<InferRouteConfig<T["GET"][K]>, "body">, "result">
-    ) => Promise<InferRouteConfig<T["GET"][K]>["result"]>;
-  };
+    ) => Promise<InferRouteConfig<T["GET"][K]>["result"]>
+  }
   POST: {
     [K in keyof T["POST"]]: (
       data: Omit<InferRouteConfig<T["POST"][K]>, "result">
-    ) => Promise<InferRouteConfig<T["POST"][K]>["result"]>;
-  };
-};
+    ) => Promise<InferRouteConfig<T["POST"][K]>["result"]>
+  }
+}
 
 export const applyConfigToExpressRouter = <T extends RouterConfig>(
   router: Router,
   config: T,
   handlers: RouterHandlerConfig<T>
 ) => {
-  const routes = Object.keys(config.GET);
-
-  routes.reduce(
+  router = Object.keys(config.GET).reduce(
     (r, x) =>
       r.get(x, async (req, res, next) => {
         try {
           const data = {
             query: config.GET[x]?.query?.parse(req.query),
-          };
-          const result = await handlers.GET[x]?.(data as any);
-          const validatedResult = config.GET[x]?.result.parse(result);
-          res.json(validatedResult);
+          }
+          const result = await handlers.GET[x]?.(data as any)
+          const validatedResult = config.GET[x]?.result.parse(result)
+          res.json(validatedResult)
         } catch (err) {
-          next(err);
+          next(err)
         }
       }),
     router
-  );
+  )
 
-  routes.reduce(
+  router = Object.keys(config.POST).reduce(
     (r, x) =>
       r.post(x, async (req, res, next) => {
         try {
           const data = {
             body: config.POST[x]?.body.parse(req.body),
             query: config.POST[x]?.query?.parse(req.query),
-          };
-          const result = await handlers.POST[x]?.(data as any);
-          const validatedResult = config.POST[x]?.result.parse(result);
-          res.json(validatedResult);
+          }
+          const result = await handlers.POST[x]?.(data as any)
+          const validatedResult = config.POST[x]?.result.parse(result)
+          res.json(validatedResult)
         } catch (err) {
-          next(err);
+          next(err)
         }
       }),
     router
-  );
+  )
 
-  return router;
-};
+  return router
+}
