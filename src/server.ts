5c26b99201b381c69805759a806f5077c19f4806
Ezeki Zibzibadze <ez@jok.io>
2026-01-03 23:35:29 +0400

    feat: add context support for the server

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,42 @@
-import type { Router } from "express"
+import type { Request, Router } from "express"
 import type { InferRouteConfig, RouterConfig } from "./types"
 
-export type RouterHandlerConfig<T extends RouterConfig> = {
+export type RouterHandlerConfig<T extends RouterConfig, TContext> = {
   GET: {
     [K in keyof T["GET"]]: (
-      data: Omit<Omit<InferRouteConfig<T["GET"][K]>, "body">, "result">
-    ) => Promise<InferRouteConfig<T["GET"][K]>["result"]>
+      data: Omit<Omit<InferRouteConfig<T["GET"][K]>, "body">, "result">,
+      ctx: TContext
+    ) =>
+      | Promise<InferRouteConfig<T["GET"][K]>["result"]>
+      | InferRouteConfig<T["GET"][K]>["result"]
   }
   POST: {
     [K in keyof T["POST"]]: (
-      data: Omit<InferRouteConfig<T["POST"][K]>, "result">
-    ) => Promise<InferRouteConfig<T["POST"][K]>["result"]>
+      data: Omit<InferRouteConfig<T["POST"][K]>, "result">,
+      ctx: TContext
+    ) =>
+      | Promise<InferRouteConfig<T["POST"][K]>["result"]>
+      | InferRouteConfig<T["POST"][K]>["result"]
   }
 }
 
-export const applyConfigToExpressRouter = <T extends RouterConfig>(
+export const applyConfigToExpressRouter = <T extends RouterConfig, TContext>(
   router: Router,
   config: T,
-  handlers: RouterHandlerConfig<T>
+  handlers: RouterHandlerConfig<T, TContext> & {
+    ctx?: (req: Request) => TContext
+  }
 ) => {
   router = Object.keys(config.GET).reduce(
     (r, x) =>
       r.get(x, async (req, res, next) => {
         try {
+          const ctx = (handlers.ctx?.(req) ?? {}) as TContext
+
           const data = {
             query: config.GET[x]?.query?.parse(req.query),
           }
-          const result = await handlers.GET[x]?.(data as any)
+          const result = await handlers.GET[x]?.(data as any, ctx)
           const validatedResult = config.GET[x]?.result.parse(result)
           res.json(validatedResult)
         } catch (err) {
@@ -40,11 +50,13 @@ export const applyConfigToExpressRouter = <T extends RouterConfig>(
     (r, x) =>
       r.post(x, async (req, res, next) => {
         try {
+          const ctx = (handlers.ctx?.(req) ?? {}) as TContext
+
           const data = {
             body: config.POST[x]?.body.parse(req.body),
             query: config.POST[x]?.query?.parse(req.query),
           }
-          const result = await handlers.POST[x]?.(data as any)
+          const result = await handlers.POST[x]?.(data as any, ctx)
           const validatedResult = config.POST[x]?.result.parse(result)
           res.json(validatedResult)
         } catch (err) {
