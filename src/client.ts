ee439cd2e085211415a8c234d15be8846942597d
Ezeki Zibzibadze <ez@jok.io>
2026-01-03 21:29:42 +0400

    feat: improve client implementation

--- src/client.ts
+++ src/client.ts
@@ -1,26 +1,25 @@
-import type { InferRouteConfig, RouterConfig } from "./types";
+import type { InferRouteConfig, RouterConfig } from "./types"
 
 export type RouterClient<T extends RouterConfig> = {
-  GET: {
-    [K in keyof T["GET"]]: (
-      data?: Omit<Omit<InferRouteConfig<T["GET"][K]>, "body">, "result">
-    ) => Promise<InferRouteConfig<T["GET"][K]>["result"]>;
-  };
-  POST: {
-    [K in keyof T["POST"]]: (
-      data: Omit<InferRouteConfig<T["POST"][K]>, "result">
-    ) => Promise<InferRouteConfig<T["POST"][K]>["result"]>;
-  };
-};
+  GET: <K extends keyof T["GET"]>(
+    path: K,
+    options?: Omit<Omit<InferRouteConfig<T["GET"][K]>, "body">, "result">
+  ) => Promise<InferRouteConfig<T["GET"][K]>["result"]>
+  POST: <K extends keyof T["POST"]>(
+    path: K,
+    body: InferRouteConfig<T["POST"][K]>["body"],
+    options?: Omit<InferRouteConfig<T["POST"][K]>, "body" | "result">
+  ) => Promise<InferRouteConfig<T["POST"][K]>["result"]>
+}
 
-type FetchFunction = (url: string, options: RequestInit) => Promise<Response>;
+type FetchFunction = (url: string, options: RequestInit) => Promise<Response>
 
 type CreateClientOptions = {
-  baseUrl: string;
-  headers?: Record<string, string>;
-  fetch?: FetchFunction;
-  validateRequest?: boolean;
-};
+  baseUrl: string
+  headers?: Record<string, string>
+  fetch?: FetchFunction
+  validateRequest?: boolean
+}
 
 export const createClient = <T extends RouterConfig>(
   config: T,
@@ -31,66 +30,70 @@ export const createClient = <T extends RouterConfig>(
     headers = {},
     fetch: customFetch = fetch,
     validateRequest = false,
-  } = options;
+  } = options
 
   const client = {
-    GET: {} as RouterClient<T>["GET"],
-    POST: {} as RouterClient<T>["POST"],
-  };
+    GET: {} as any,
+    POST: {} as any,
+  }
 
-  Object.keys(config.GET).forEach((path) => {
-    (client.GET[path as keyof T["GET"]] as any) = async (data?: any) => {
-      if (validateRequest && data?.query) {
-        config.GET[path]?.query?.parse(data.query);
+  client.GET = async (path: string, data?: any) => {
+    if (validateRequest && data?.query) {
+      config.GET[path]?.query?.parse(data.query)
+    }
+    const queryString = data?.query
+      ? "?" + new URLSearchParams(data.query).toString()
+      : ""
+    const response = await customFetch(`${baseUrl}${path}${queryString}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+    })
+
+    if (!response.ok) {
+      const error: any = await response.json()
+      console.error(error)
+      throw new Error(error.message)
+    }
+
+    const json = await response.json()
+
+    return config.GET[path]?.result.parse(json)
+  }
+
+  client.POST = async (path: string, body: any, rest?: any) => {
+    if (validateRequest) {
+      if (body) {
+        config.POST[path]?.body?.parse(body)
       }
-      const queryString = data?.query
-        ? "?" + new URLSearchParams(data.query).toString()
-        : "";
-      const response = await customFetch(`${baseUrl}${path}${queryString}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          ...headers,
-        },
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      if (rest?.query) {
+        config.POST[path]?.query?.parse(rest.query)
       }
-      const json = await response.json();
+    }
+    const queryString = rest?.query
+      ? "?" + new URLSearchParams(rest.query).toString()
+      : ""
+    const response = await customFetch(`${baseUrl}${path}${queryString}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+      body: JSON.stringify(body),
+    })
 
-      return config.GET[path]?.result.parse(json);
-    };
-  });
+    if (!response.ok) {
+      const error: any = await response.json()
+      console.error(error)
+      throw new Error(error.message)
+    }
 
-  Object.keys(config.POST).forEach((path) => {
-    (client.POST[path as keyof T["POST"]] as any) = async (data: any) => {
-      if (validateRequest) {
-        if (data?.body) {
-          config.POST[path]?.body?.parse(data.body);
-        }
-        if (data?.query) {
-          config.POST[path]?.query?.parse(data.query);
-        }
-      }
-      const queryString = data?.query
-        ? "?" + new URLSearchParams(data.query).toString()
-        : "";
-      const response = await customFetch(`${baseUrl}${path}${queryString}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          ...headers,
-        },
-        body: JSON.stringify(data.body),
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const json = await response.json();
+    const json = await response.json()
 
-      return config.POST[path]?.result.parse(json);
-    };
-  });
+    return config.POST[path]?.result.parse(json)
+  }
 
-  return client;
-};
+  return client
+}
